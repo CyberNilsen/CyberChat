74e653466cbd44915435970de7e1550742a9fb4b
Barnegraver <andreasnilsen07@hotmail.com>
2025-06-12 18:23:39 +0200

    Added more images to the program and also made that the program uses the logos/images

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setApplicationName("CyberChat");
+    a.setApplicationVersion("1.0");
+    a.setOrganizationName("CyberNilsen");
+    a.setApplicationDisplayName("CyberChat - Secure Anonymous Messaging");
+
+    a.setWindowIcon(QIcon(":/logo.png"));
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
-
 // Here is what happens when we start the program, we initilize the window and show it, we also keep it running when it has started
